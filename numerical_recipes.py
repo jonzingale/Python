36997a4f84bcae29e10b99cbcd96aef2e3b1c4a7
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-13 12:46:03 -0600

    some numerical operators

--- numerical_recipes.py
+++ numerical_recipes.py
@@ -4,19 +4,29 @@ import pdb
 
 def pry(): pdb.set_trace()
 
+def div(n, d, i=0):
+  while n >= d:
+    n -= d
+    i += 1
+  return i
+
+def mod(n, d):
+  return n - div(n, d) * d
+
+def gcd(a, b):
+  if b == 0: return a
+  else: return gcd(b, a % b)
+
 def triangle_num(xs):
-  ary = range(1, xs + 1)
-  return reduce(operator.add, ary, 0)
+  return reduce(operator.add, range(xs), 0)
 
 def triangle_num2(xs):
-  ary = range(1, xs + 1)
-  return reduce(lambda a, b: a + b, ary, 0)
+  return reduce(lambda a, b: a + b, range(xs), 0)
 
 def qsort(xs):
   if (len(xs) < 1): return []
   else:
     return qsort(less(xs[0], xs[1:])) + xs[:1] + qsort(more_eq(xs[0], xs[1:]))
-
 
 def less(x, xs):
   return list(filter(lambda t: t < x, xs))
@@ -24,11 +34,13 @@ def less(x, xs):
 def more_eq(x, xs):
   return list(filter(lambda t: t >= x, xs))
 
+ary = map(lambda x: 10-x, range(10))
+lamb = (lambda x: x+1)(2)
 
-ary=map(lambda x: 10-x, range(1,11))
-
-print triangle_num(5)
-print triangle_num2(3)
-print qsort(ary)
-
+# print triangle_num(5)
+# print triangle_num2(3)
+# print qsort(ary)
+# print div(10,2)
+# print mod(10,3)
+print gcd(36, 9)
 # pry()

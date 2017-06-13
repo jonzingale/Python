a34cd0b350451614c8b77238e900fd50b5d85c06
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-12 20:01:49 -0600

    triangle numbers

--- numerical_recipes.py
+++ numerical_recipes.py
@@ -1,8 +1,17 @@
 # Herein live Numerical Recipes
+import operator
 import pdb
 
-def array():
-  pdb.set_trace()
-  [1,2,3]
+def pry(): pdb.set_trace()
 
-myList=array
+def triangle_num(xs):
+  ary = range(1, xs + 1)
+  return reduce(operator.add, ary, 0)
+
+def triangle_num2(xs):
+  ary = range(1, xs + 1)
+  return reduce(lambda a, b: a+b, ary, 0)
+
+# pry()
+print triangle_num(5)
+print triangle_num2(3)

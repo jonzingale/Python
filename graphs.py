03c7a1595f98644558a61151c923d3051dee477e
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-14 13:38:01 -0600

    havelhakimi

--- graphs.py
+++ graphs.py
@@ -2,23 +2,16 @@ from listables import reduce
 from operator import add, mul, mod, pow
 
 import pdb
-# Haskell:
-# havelhakimi :: [Int] -> Bool
-# havelhakimi (a:[]) = a == 0
-# havelhakimi (a:as) = havelhakimi.qsort $ 
-#   map (+ (-1)) (take a as) ++ drop a as
-
 
 # recursive HH
 def havelhakimi(xs):
   if len(xs) == 1:
-    return (0 == xs[0]) # print works why not return?
+    return (0 == xs[0])
   else:
     (head, tail) = xs[0], xs[1:]
     mlist = list(map(lambda x: x-1, tail[:head]))
     tlist = mlist + tail[head:]
-    havelhakimi(qsort(tlist))
-
+    return havelhakimi(qsort(tlist))
 
 def qsort(xs):
   if (len(xs) < 1): return []
@@ -31,6 +24,5 @@ def less(x, xs):
 def more_eq(x, xs):
   return list(filter(lambda t: t >= x, xs))
 
-it = havelhakimi([2,3,4,3,2])
-print(it)
+print(havelhakimi([2,3,4,3,2]))
 

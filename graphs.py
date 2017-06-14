fa87c7797b482f6dedf41fa6c081e7c16aa93f79
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-14 14:00:13 -0600

    clean up

--- graphs.py
+++ graphs.py
@@ -10,19 +10,9 @@ def havelhakimi(xs):
   else:
     (head, tail) = xs[0], xs[1:]
     mlist = list(map(lambda x: x-1, tail[:head]))
-    tlist = mlist + tail[head:]
-    return havelhakimi(qsort(tlist))
+    mlist += tail[head:] ; mlist.sort()
+    return havelhakimi(mlist)
 
-def qsort(xs):
-  if (len(xs) < 1): return []
-  else:
-    return qsort(less(xs[0], xs[1:])) + xs[:1] + qsort(more_eq(xs[0], xs[1:]))
-
-def less(x, xs):
-  return list(filter(lambda t: t < x, xs))
-
-def more_eq(x, xs):
-  return list(filter(lambda t: t >= x, xs))
-
-print(havelhakimi([2,3,4,3,2]))
+print(havelhakimi([2,3,4,3,2])) # no Erdos graphs
+print(havelhakimi([2,1,1])) # yes Erdos graphs
 

2be8c321ac702da341c7677ac01d7126c5af6ebd
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-14 12:32:39 -0600

    clean up

--- listables.py
+++ listables.py
@@ -1,15 +1,8 @@
-import operator
+from operator import add, mul, mod, pow
 import pdb
 
 def pry(): pdb.set_trace()
 
-def to_a(rs):
-  accum = []
-  for i in rs: accum.append(i)
-  return accum
-
 def reduce(opr, ls, base):
   for i in ls: base = opr(base, i)
   return base
-
-trinum = reduce(operator.add, to_a(range(4)), 0)

2dbc5bba52e435ba30f100a0899fd716eb317514
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-18 20:50:23 -0600

    added key shuffle

--- listables.py
+++ listables.py
@@ -7,3 +7,17 @@ def pry(): pdb.set_trace()
 def reduce(opr, ls, base):
   for i in ls: base = opr(base, i)
   return base
+
+# Shuffling #
+def key_shuffle(deck):
+  size = len(deck)
+  zipped = list(zip(rands(size), deck))
+  zipped.sort()
+  return list(zip(*zipped))[1]
+
+def rands(n):
+  ary = []
+  logn = math.log(n**3,2)
+  for i in range(n):
+    ary.append(getrandbits(math.floor(logn)))
+  return ary

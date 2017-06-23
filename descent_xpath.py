b4a9445e656d311ce332dfc400e61908f7ee429d
Jonathan Zingale <jonzingale@gmail.com>
2017-06-23 06:41:31 -0600

    clean up

--- descent_xpath.py
+++ descent_xpath.py
@@ -5,6 +5,7 @@ import requests
 page = requests.get('http://www.weathercurrents.org')
 tree = html.fromstring(page.content)
 
+# prices = tree.xpath('//span[@class="item-price"]/text()')
 weathercurrents = tree.xpath('//span/text()')[-1]
 
 print(weathercurrents)

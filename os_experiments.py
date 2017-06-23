f2e8eeadc882572e08341e72df5f294d43ad5740
Jonathan Zingale <jonzingale@gmail.com>
2017-06-23 06:34:03 -0600

    towards file system usage

---
+++ os_experiments.py
@@ -0,0 +1,20 @@
+from pprint import pprint as pp
+from pdb import set_trace as st
+import os
+import re
+
+blacklist = '''\.|HEAD|COMMIT|update.sample|\w+\-(commit|rebase
+                 |push|apply|post)|master|index|config'''
+
+def contents_of_crude():
+  ff = []
+  for root, dir, files in os.walk('./../'):
+    for file in files:
+      if len(file) < 38 and not re.match(blacklist, file):
+        ff.append(file)
+
+  ff.sort()
+  pp(ff)
+
+
+contents_of_crude()

67b9ef623224672028611e1cb9804b23570a8fdb
Jonathan Zingale <jonzingale@gmail.com>
2017-06-23 05:42:14 -0600

    updated simple weather scraper

--- simple_weather_scraper.py
+++ simple_weather_scraper.py
@@ -5,8 +5,6 @@ import descarteslabs as dl
 import urllib3
 import lxml
 import os
-import re
-
 
 lat_long = 'lat=35.6879&lon=-105.9447'
 
@@ -18,8 +16,8 @@ NWS_HEADER = {'Accept' : 'application/vnd.noaa.dwml+xml;version=1'}
 # see descartes labs docs for places.find
 def get_coords_url(place):
   dl_taos = dl.places.find(place)
-  lat, long = dl_taos[0]['bbox'][1:3]
-  return(NWS_URL % (lat, long))
+  lat , lng = dl_taos[0]['bbox'][1:3]
+  return(NWS_URL % (lat, lng))
 
 def get_santa_fe():
   santa_fe = get_coords_url('new-mexico_taos')
@@ -40,17 +38,9 @@ def post_form(url, params={}):
   return(soup(data, "html.parser"))
 
 def main():
-  # page = get_page(NWS_URL)
-  # action = page.form['action']
-  # agent = urllib3.PoolManager(10, headers=USER_AGENT)
-  # action = 'http://forecast.weather.gov/zipcity.php'
-  # headers = {'Accept' : 'application/vnd.noaa.dwml+xml;version=1'}
-  # query = {'inputstring': '87501'}
-  # print(agent.request('GET', action, fields=query, headers=headers).data)
-
   sf = get_santa_fe()
 
-  # temp max and min
+  # nws weather data
   temp_max = sf.find('p', class_='myforecast-current-lrg').text
   temp_min = sf.find('p', class_='myforecast-current-sm').text
   details = sf.find('div', id="current-conditions-body")

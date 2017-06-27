8be1a2b3a7fbc2db647998b6751f2d1bcf72a403
Jon Zingale <jon@jackrabbitsystems.com>
2017-06-26 22:23:47 -0600

    added bigInteger to model

--- django/tensorflow_nlp/books/models.py
+++ django/tensorflow_nlp/books/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# https://catalog.loc.gov/vwebv/search?
-# str.split('/')[0].strip()
-
-# b = Book(title=str, author='S. Mac Lane',lccn='QA169 .M33 1994', isbn='0387900357', pub_date = '1994')
+# b = Book(title=str, author='S. Mac Lane',lccn='QA169 .M33 1994', isbn='0387900357', pub_year = '1994')
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     lccn = models.CharField(max_length=200)
-    isbn = models.IntegerField(default=0)
+    isbn = models.BigIntegerField(default=0)
     pub_year = models.IntegerField(default=0)
+
+    def __str__(self):
+      return self.title

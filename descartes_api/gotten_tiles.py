952ad679c44517de7b5c416f5e6f060b600ad225
Jon Zingale <jonzingale@gmail.com>
2017-07-24 16:34:43 -0600

    connors tiles for cuyahoga

--- descartes_api/gotten_tiles.py
+++ descartes_api/gotten_tiles.py
@@ -1,9 +1,6 @@
-import os
-from pprint import pprint
 import descarteslabs as dl
-dl.raster.url = 'https://platform-services.descarteslabs.com/raster/dev'
-import sys
-sys.path.append('/Users/conor/anaconda3/envs/descartes/Lib/site-packages') # yay windows %matplotlib inline
+
+from pprint import pprint
 import matplotlib.pyplot as plt
 import shapely.geometry
 import cartopy
@@ -13,18 +10,18 @@ import numpy as np
 
 ctown = dl.places.shape('ohio_cuyahoga')
 tiles = dl.raster.dltiles_from_shape(30.0, 2048, 16, ctown)
-pprint(tiles['features'][0])
-
+# pprint(tiles['features'][0])
 
 lonlat_crs = cartopy.crs.PlateCarree()
-albers = cartopy.crs.AlbersEqualArea(central_latitude=41.0, central_longitude=-71)
+albers = cartopy.crs.AlbersEqualArea(central_latitude=41.3, central_longitude=-82)
 fig = plt.figure(figsize=(6, 8))
 ax = plt.subplot(projection=albers) # Specify projection of the map here
-ax.add_geometries([shapely.geometry.shape(ctown['geometry'])],
-                   lonlat_crs)
+ax.add_geometries([shapely.geometry.shape(ctown['geometry'])], lonlat_crs)
+
 # Get the geometry from each feature
 shapes = [shapely.geometry.shape(f['geometry']) for f in tiles['features']]
 ax.add_geometries(shapes, lonlat_crs, alpha=0.3, color='green')
+
 # Get a bounding box of the combined scenes
 union = shapely.geometry.MultiPolygon(polygons=shapes)
 bbox = union.bounds

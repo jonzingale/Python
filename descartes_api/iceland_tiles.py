e781f5fd0bc3141d38eca38a7c449466228c8b7c
Jon Zingale <jonzingale@gmail.com>
2017-07-24 16:30:15 -0600

    toward volcanic activity

--- descartes_api/iceland_tiles.py
+++ descartes_api/iceland_tiles.py
@@ -10,90 +10,61 @@ import os
 
 from inside_outside import pointed_region
 
+# Find potential AOI matches
+place = 'iceland'
+matches = dl.places.find(place)
+if not matches: st()
+aoi = matches[0]
+location = dl.places.shape(place)
+geocoords = [-19.6271567, 63.6045345]
+
 def get_feature_collection(const_id, aoi):
-  # rand_date = datetime.date(2016, randint(8,9), randint(1,28)) # May - August
-  rand_date = datetime.date(2013, 6, 3)
+  rand_date = datetime.date(2016, randint(1,12), randint(1,28))
   rand_start = rand_date.strftime('%Y-%m-%d')
-  rand_end = (rand_date + datetime.timedelta(days=10)).strftime('%Y-%m-%d')
+  rand_end = (rand_date + datetime.timedelta(days=30)).strftime('%Y-%m-%d')
   print(rand_start)
 
   feature_collection = dl.metadata.search(const_id=const_id,
                                           start_time=rand_start,
                                           end_time=rand_end,
                                           limit=10, place=aoi['slug'])
-  return(feature_collection)
+  return(feature_collection, rand_start)
 
-def save_image(aoi, tile):
+def save_image(aoi, tile, rand_start):
   mu_sec = datetime.datetime.time(datetime.datetime.now()).microsecond
-
-  # Check that directory exists. If not, make it.
-  dir_name = "./images/%s" % aoi['name']
-  dir_cond = os.path.isdir(dir_name)
-  if not dir_cond: os.mkdir(dir_name)
-
-  fst_coords = tile['geometry']['coordinates'][0][0]
-  coord_name = ('_').join([str(it) for it in fst_coords])
-
-  filename = "./images/%s/%s.png" % (aoi['name'], coord_name)
-  savefig(filename, dpi=300, facecolor='k')
-  time.sleep(3)
+  filename = "./images/%s/volcano/%s_%s.png" % (aoi['name'], rand_start, mu_sec)
+  savefig(filename, dpi=500, facecolor='k')
+  time.sleep(2)
   if os.stat(filename).st_size < 1*10**6: os.remove(filename)
 
-# Find potential AOI matches
-# place = 'new-mexico_santa-fe'
-# place = 'cuba'
-place = 'new-mexico_sandoval'
-matches = dl.places.find(place)
-if not matches: st()
-aoi = matches[0]
-location = dl.places.shape(place)
+def get_images():
+  # make sure we have features.
+  feature_collection, rand_start = get_feature_collection('L8SR', aoi)
+  while not feature_collection['features']:
+    feature_collection, rand_start = get_feature_collection('L8SR', aoi)
 
-# resolution, tilesize, pad
-tiles = dl.raster.dltiles_from_shape(5, 4000, 16, location) # 2048
 
-# make sure we have features.
-feature_collection = get_feature_collection('L8SR', aoi)
-while not feature_collection['features']:
-  feature_collection = get_feature_collection('L8SR', aoi)
+  # resolution, tilesize, pad
+  # tiles = dl.raster.dltiles_from_shape(5, 4000, 16, location) # 2048
 
-shuffle(tiles['features'])
+  pr = pointed_region(geocoords, place, 5, 4000)
+  tiles = pr.possible_regions()
 
-# russ = (47.8496568,-121.9752158)
-pprint(len(tiles['features']))
+  for tile in tiles:
+    ids = [f['id'] for f in feature_collection['features']]
 
-# fire_coords3 = ['-106.7709367384569, 35.77031597485538']
-# fire_coords1 = [-106.33291249604501, 35.772363328866696]
-# fire_coords2 = [-106.99641235353315, 35.76317763618165]
-# fire_coords = [fire_coords1, fire_coords2]
+    arr, meta = dl.raster.ndarray(
+        ids,
+        bands=['swir2','swir1','red','bai'],
+        scales=[[0,4000],[0,4000],[0,4000],[0, 65535]],
+        data_type='Byte',# Float32' 'Float64' 'Byte'
+        resolution=5,
+        cutline=tile['geometry'],
+    )
+    plt.figure(figsize=[7,7], facecolor='k') # does not affect save.
+    plt.axis('off')
+    plt.imshow(arr)
+    save_image(aoi, tile, rand_start)
 
-# fire_tiles = []
-# for coord in fire_coords:
-#   region = pointed_region(coord, place).possible_regions()
-#   if region: fire_tiles.append(region)
-
-# for tile in fire_tiles:
-
-for tile in tiles['features']:
-
-  ids = [f['id'] for f in feature_collection['features']]
-  # st()
-# ('L8SR', ['red', 'green', 'blue', 'cloud', 'bright',
-# 'qa_snow', 'qa_water', 'qa_cirrus', 'qa_cloud', 'nir',
-# 'swir1', 'swir2', 'aerosol', 'cirrus', 'thermal', 'sr_cirrus',
-# 'sr_cloud', 'sr_adjacent_cloud', 'sr_cloud_shadow', 'sr_water',
-# 'aerosol_level', 'ipflags_enum', 'alpha', 'evi', 'visual_cloud_mask',
-# 'rsqrt', 'ndwi2', 'ndwi1', 'ndvi', 'ndwi', 'bai']), 
-  arr, meta = dl.raster.ndarray(
-      ids,
-      # keep frequencies low to high?
-      bands=['swir2','swir1','red','bai'],
-      scales=[[0,4000],[0,4000],[0,4000],[0, 65535]],
-      data_type='Byte',# Float32' 'Float64' 'Byte'
-      resolution=5,
-      cutline=tile['geometry'],
-  )
-  plt.figure(figsize=[7,7], facecolor='k')
-  plt.axis('off')
-  plt.imshow(arr)
-  save_image(aoi, tile)
+for i in range(15): get_images()
 plt.show()

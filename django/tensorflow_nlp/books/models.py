56a3c7693870f3db6ce50191786e273278ca60a3
Jon Zingale <jonzingale@gmail.com>
2017-08-04 15:15:10 -0600

    indentation fixup

--- django/tensorflow_nlp/books/models.py
+++ django/tensorflow_nlp/books/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    lccn = models.CharField(max_length=200)
-    isbn = models.BigIntegerField(default=0)
-    pub_year = models.IntegerField(default=0)
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=200)
+  lccn = models.CharField(max_length=200)
+  isbn = models.BigIntegerField(default=0)
+  pub_year = models.IntegerField(default=0)
 
-    def __str__(self):
-      return self.title
+  def __str__(self):
+    return self.title

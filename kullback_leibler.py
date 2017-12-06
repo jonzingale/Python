457446f0790447cfb83d42ab65f6674ddd9ab784
Jon Zingale <jonzingale@gmail.com>
2017-12-06 09:49:12 -0700

    metrics for the biases of witnesses to probabilistic primes

--- kullback_leibler.py
+++ kullback_leibler.py
@@ -26,6 +26,16 @@ def witnessTo(p):
 			testimony.append(E)
 	return(testimony)
 
+# gives the witnesses bias
+def fromWitness(a):
+	testimony = []
+	for p in range(2,10**4):
+		if a**p % p == a:
+			testimony.append(1)
+		else:
+			testimony.append(E)
+	return(testimony)
+
 # normalize the dimension of both arrays
 def normalize(a, b):
 	if len(a) > len(b):
@@ -36,7 +46,19 @@ def normalize(a, b):
 			a.append(E)
 	return(a, b)
 
+# determines distances for a witnesses bias
+# to the probable primality of numbers
+def klWitnesses(a, b):
+	aa = fromWitness(a)
+	bb = fromWitness(b)
+	aNorm, bNorm = normalize(aa, bb)
 
+	wass = wasserstein_distance(aNorm, bNorm)
+	energy = energy_distance(aNorm, bNorm)
+	kl = entropy(aNorm, bNorm)
+	return([wass, energy, kl])
+
+# determines distances of probable primes
 def klPrimes(a, b):
 	aa = witnessTo(a)
 	bb = witnessTo(b)
@@ -47,11 +69,17 @@ def klPrimes(a, b):
 	kl = entropy(aNorm, bNorm)
 	return([wass, energy, kl])
 
+print('klPrimes')
 print(['wasserstein_distance','energy_distance','kullback-leibler'])
 print(klPrimes(3, 167))
 print(klPrimes(3, 17))
 print(klPrimes(30, 17))
 
+print('\nklWitnesses')
+print(['wasserstein_distance','energy_distance','kullback-leibler'])
+print(klWitnesses(3, 167))
+print(klWitnesses(3, 17))
+print(klWitnesses(30, 17))
 
 
 # st()

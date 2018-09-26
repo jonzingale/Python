3a8a6d805efec3a6bbf268d093495cf9f0f9b87c
Jon Zingale <jonzingale@gmail.com>
2018-09-26 11:18:45 -0600

    towards csv conversion

---
+++ SFI/xlxsToCsv.py
@@ -0,0 +1,16 @@
+import xlrd
+import csv
+
+def csv_from_excel():
+    wb = xlrd.open_workbook('excel.xlsx')
+    sh = wb.sheet_by_name('Sheet1')
+    your_csv_file = open('your_csv_file.csv', 'w')
+    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    your_csv_file.close()
+
+# runs the csv_from_excel function:
+csv_from_excel()

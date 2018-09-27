68875495ec8d338b32255d0669e8850a2603add3
Jon Zingale <jonzingale@gmail.com>
2018-09-27 09:32:52 -0600

    robots and sitemap

--- SFI/xlxsToCsv.py
+++ SFI/xlxsToCsv.py
@@ -1,16 +1,51 @@
+# https://xlrd.readthedocs.io/en/latest/api.html
+from pdb import set_trace as st
 import xlrd
 import csv
+import re
+
+HEADER = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
 def csv_from_excel():
-    wb = xlrd.open_workbook('excel.xlsx')
-    sh = wb.sheet_by_name('Sheet1')
-    your_csv_file = open('your_csv_file.csv', 'w')
-    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+    wb = xlrd.open_workbook('seoAudit.xlsx')
 
-    for rownum in range(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
+    for sh in wb.sheets():
+      your_csv_file = open('audit/'+sh.name+'.csv', 'w')
+      wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
-    your_csv_file.close()
+      for rownum in range(sh.nrows):
+          wr.writerow(sh.row_values(rownum))
 
-# runs the csv_from_excel function:
-csv_from_excel()
+      your_csv_file.close()
+
+# returns all sheets as csvs:
+# csv_from_excel()
+
+def urlsToSitemap():
+  file = open("audit/urls.csv", "r")
+  body = ""
+  for url in file:
+    body += '<url><loc>' + url + '</loc><lastmod>2018-09-26</lastmod></url>'
+  xml = HEADER + body + '</urlset>'
+
+  file = open("sitemap_1.xml", "w")
+  file.write(xml)
+  file.close()
+
+# compose sitemap from valid urls
+# urlsToSitemap()
+
+def getUrls():
+  wb = xlrd.open_workbook('seoAudit.xlsx')
+  file = open("urls.csv", "a")
+
+  urlText = wb.sheets()[0].col(0)[1:]
+  urls = []
+  for u in urlText:
+    mm = re.match("text:\'(.*)\'", str(u))
+    if mm: file.write(mm[1]+"\n")
+  file.close()
+
+# get Urls from excel and create csv
+getUrls()
+# st()

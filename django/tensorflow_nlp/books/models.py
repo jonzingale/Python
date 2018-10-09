52a187256d4648100cb1cacd40306409bef10152
Jon Zingale <jonzingale@gmail.com>
2018-10-08 20:24:13 -0600

    removed unnecessary model cruft

--- django/tensorflow_nlp/books/models.py
+++ django/tensorflow_nlp/books/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from django.contrib.sites.models import Site
 from django.contrib.sitemaps import Sitemap
-from django.core.urlresolvers import reverse
 
 class BookSiteMap(Sitemap):
     changefreq = "never"
@@ -24,9 +23,6 @@ class Book(models.Model):
   lccn = models.CharField(max_length=200)
   isbn = models.BigIntegerField(default=0)
   pub_year = models.IntegerField(default=0)
-
-  # def get_absolute_url(self):
-      # return reverse('book', kwargs={'name':self.title})
 
   def __str__(self):
     return self.title
